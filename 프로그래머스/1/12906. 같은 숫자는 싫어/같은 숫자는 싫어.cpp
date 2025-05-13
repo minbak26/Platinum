a210860a2385b9829498da51d2efa41363e195fc
minbak26 <103039794+minbak26@users.noreply.github.com>
2025-05-13 11:32:20 +0900

    Update 같은 숫자는 싫어.cpp

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -3,18 +3,13 @@
 
 using namespace std;
 
-vector<int> solution(vector<int> arr) 
+vector<int> solution(vector<int> arr)
 {
     vector<int> answer;
-    int arrLength = arr.size();
-
-    for (int i = 0; i < arrLength; i++)
+    for (int i = 0; i < arr.size(); i++)
     {
-        if (i == 0 || arr[i] != arr[i - 1]) 
-        {
+        if(!find(0, i, arr[i]))
             answer.push_back(arr[i]);
-        }
     }
-
     return answer;
 }
